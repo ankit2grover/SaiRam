7d2bad10e5f6383cb32255d6464321474d8f8b41
ANKIT GROVER <ankitgrover@ANKITs-MacBook-Pro.local>
2017-03-21 01:20:38 -0700

    Udacity Assignments

---
+++ MachineLearningDemos/GoogleUdacityAssignments/Assignment5Word2VecAnkit.py
@@ -0,0 +1,40 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import collections
+import math
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import zipfile
+from matplotlib import pylab
+from six.moves import range
+from six.moves.urllib.request import urlretrieve
+from sklearn.manifold import TSNE
+
+url = 'http://mattmahoney.net/dc/'
+
+def maybe_download(filename, expected_bytes):
+
+	if not os.path.exists(filename):
+		filename, _ = urlretrieve(url + filename, filename)
+	statinfo = os.stat(filename)
+	if (statinfo.st_size == expected_bytes):
+		print ("Found and verified %s" % filename)
+	else:
+		print ("Found size %s" % statinfo.st_size)
+		raise Exception("Failed to verify " + filename + ". Can you get to it with a browser?")
+	return filename
+
+filename = maybe_download("text8.zip", 31344016)
+
+def read_data(filename):
+	"""Extract the first file enclosed in a zip file as a list of words"""
+	with zipfile.ZipFile(filename) as f:
+		data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+  	return data
+
+words = read_data(filename)
+print('Data size %d' % len(words))
+print ('Split words %s' % words[0])
